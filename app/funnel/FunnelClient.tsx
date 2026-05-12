71d94d72393ef526196a2d028b652b0431bf5ffd
nico1523 <nicochaves1523@gmail.com>
2026-05-12 13:34:40 -0400

    auto-tag funnel optins

--- app/funnel/FunnelClient.tsx
+++ app/funnel/FunnelClient.tsx
@@ -138,6 +138,7 @@ export default function FunnelClient() {
             email,
             first_name: name,
             fields: { phone },
+            tags: [18774682],
           }),
         })
       }
